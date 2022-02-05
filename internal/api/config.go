b8f01fecf5372dcf6a35fefa36866698dedaa2bf
#MDanial <48054961+mdanialr@users.noreply.github.com>
2022-02-06 05:34:47 +0700

    add api constant and config that implement bare minimum specification from BBB API and squash (#2)
    * add service to SHA1 hash the given url
    
    * add api constant and config that implement bare minimum specification from BBB API

---
+++ internal/api/config.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Interface signature to implement this api config.
+type Interface interface {
+	Sanitization() error
+}
+
+// Config holds BBB api-related data.
+type Config struct {
+	Secret string `yaml:"secret"`
+	Host   string `yaml:"host"`
+	URL    string
+}
+
+// Sanitization check and sanitize api config instance.
+func (c *Config) Sanitization() error {
+	if c.Secret == "" {
+		return fmt.Errorf("`secret` field is required")
+	}
+
+	if c.Host == "" {
+		return fmt.Errorf("`host` field is required")
+	}
+
+	if !strings.HasSuffix(c.Host, "/") {
+		c.Host += "/"
+	}
+
+	c.URL = fmt.Sprintf("%s%s", c.Host, EndPoint)
+
+	return nil
+}
---
+++ internal/api/string.go
@@ -0,0 +1,24 @@
+package api
+
+const (
+	Create              = "create"              // Creates a new meeting.
+	GetDefaultConfig    = "getDefaultConfigXML" // Gets the default config.xml (these settings configure the BigBlueButton client for each user).
+	SetConfig           = "setConfigXML"        // Add a custom config.xml to an existing meeting.
+	Join                = "join"                // Join a new user to an existing meeting.
+	End                 = "end"                 // Ends meeting.
+	IsMeetingRun        = "isMeetingRunning"    // Checks whether if a specified meeting is running.
+	GetAllMeetings      = "getMeetings"         // Get the list of Meetings.
+	GetMeetingDetail    = "getMeetingInfo"      // Get the details of a Meeting.
+	GetAllRecordings    = "getRecordings"       // Get a list of recordings.
+	DeleteRecording     = "deleteRecordings"    // Deletes an existing recording.
+	UpdateRecordingMeta = "updateRecordings"    // Updates metadata in a recording.
+	EndPoint            = "bigbluebutton/api"   // BBB API endpoint.
+)
+
+// StdResponse standard response that would always return by BBB API
+// at every API call.
+type StdResponse struct {
+	Code   string `xml:"returncode"`
+	Msg    string `xml:"message"`
+	MsgKey string `xml:"messageKey"`
+}
