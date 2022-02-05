db22962c7e44351090535bbdb7ceebc74b0fcca1
mdanialr <mdanialrma@gmail.com>
2022-02-06 04:19:26 +0700

    add service to SHA1 hash the given url

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"strings"
+)
+
+// SHA1HashUrl hash given url with the secret and return the result. '?' char
+// in url would be cleaned if any.
+func SHA1HashUrl(sc, in string) string {
+	in = strings.ReplaceAll(in, "?", "")
+	in += sc
+
+	out := sha1.Sum([]byte(in))
+	return hex.EncodeToString(out[:])
+}
--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSHA1HashUrl(t *testing.T) {
+	const SECRET = "secret"
+
+	testCases := []struct {
+		name     string
+		url      string
+		expected string
+	}{
+		{
+			name:     "Success testing w plain string",
+			url:      "thisshouldbehashed",
+			expected: "ceb03b75323a3ae65a351130210396558ade157d",
+		},
+		{
+			name:     "Success testing w url string",
+			url:      "somenameurl?param1=val1&param2=val2",
+			expected: "a81e64fc67c9db24d1c64c78dfefba035e64a741",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d# %s", i+1, tc.name), func(t *testing.T) {
+			out := SHA1HashUrl(SECRET, tc.url)
+			assert.Equal(t, tc.expected, out)
+		})
+	}
+}
