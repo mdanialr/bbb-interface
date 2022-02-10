be2ecf536fd64afeeee5b0a4b1741e59fe8d5c3a
mdanialr <mdanialrma@gmail.com>
2022-02-10 21:34:48 +0700

    add struct to implement random string

--- internal/service/random_string_test.go
+++ internal/service/random_string_test.go
@@ -7,8 +7,9 @@ import (
 )
 
 func TestRandString(t *testing.T) {
-	t.Run("Random string's lenght should equal as in param", func(t *testing.T) {
-		out := RandString(5)
+	t.Run("Random string's length should equal as in param", func(t *testing.T) {
+		r := RandomString{5}
+		out := r.RandString()
 
 		assert.Equal(t, 5, len(out))
 	})
