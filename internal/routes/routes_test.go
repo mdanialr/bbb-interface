8e89a3eb1467740a854499fec8d5467d95f79db9
mdanialr <mdanialrma@gmail.com>
2022-02-11 06:17:04 +0700

    add new handler to set up routes

--- internal/routes/routes_test.go
+++ internal/routes/routes_test.go
@@ -1,24 +1,28 @@
 package routes
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/kurvaid/bbb-interface/internal/config"
 )
 
+var fakeServer = httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {}))
+
 func TestSetupRoutes(t *testing.T) {
 	t.Run("1# Success test even there is no value supplied because there is no required value", func(t *testing.T) {
 		conf := config.Model{}
 		app := fiber.New()
 
-		SetupRoutes(app, &conf)
+		SetupRoutes(app, &conf, fakeServer.Client())
 	})
 
 	t.Run("2# Success test with only one or more supplied value", func(t *testing.T) {
 		conf := config.Model{EnvIsProd: true}
 		app := fiber.New()
 
-		SetupRoutes(app, &conf)
+		SetupRoutes(app, &conf, fakeServer.Client())
 	})
 }
