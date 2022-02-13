ed743085480988f5d81c3eba3e9a9cb6c74143f0
mdanialr <mdanialrma@gmail.com>
2022-02-13 21:54:57 +0700

    omit BBB API response from the requester response

--- internal/api/client_create.go
+++ internal/api/client_create.go
@@ -20,7 +20,7 @@ type CreateMeeting struct {
 
 // CreateMeetingResponse holds data from BBB API response after create meeting.
 type CreateMeetingResponse struct {
-	StatusCode    string `xml:"returncode"`
+	StatusCode    string `xml:"returncode" json:"-"`
 	MeetingId     string `xml:"meetingID" json:"meeting_id"`
 	AttendeePass  string `xml:"attendeePW" json:"attendee_pass"`
 	ModeratorPass string `xml:"moderatorPW" json:"moderator_pass"`
