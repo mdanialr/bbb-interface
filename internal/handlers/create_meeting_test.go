df797de8e30eeb924ae443c883c0b0467b94cd35
mdanialr <mdanialrma@gmail.com>
2022-02-18 13:19:57 +0700

    test: increase coverage in handlers pkg

--- internal/handlers/create_meeting_test.go
+++ internal/handlers/create_meeting_test.go
@@ -110,6 +110,8 @@ func TestCreateMeeting_FailedUsingEmptyJSONRequest(t *testing.T) {
 	conf, err := config.NewConfig(bytes.NewBufferString(sampleConfigFile[0]))
 	require.NoError(t, err)
 	require.NoError(t, conf.Sanitization())
+	conf.BBB.Host = fakeServerHelper(t).URL
+	require.NoError(t, conf.BBB.Sanitization())
 
 	app := fiber.New()
 	app.Post("/meeting", CreateMeeting(conf, fakeServerHelper(t).Client()))
@@ -123,5 +125,56 @@ func TestCreateMeeting_FailedUsingEmptyJSONRequest(t *testing.T) {
 			t.Fatalf("failed to initiate app test: %s", err)
 		}
 		assert.Equal(t, fiber.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("Failed if `name` field not provided", func(t *testing.T) {
+		buf := bytes.NewBufferString(`{"key": "value"}`)
+		req := httptest.NewRequest(fiber.MethodPost, "/meeting", buf)
+		req.Header.Set("Content-Type", fiber.MIMEApplicationJSON)
+		res, err := app.Test(req)
+		if err != nil {
+			t.Fatalf("failed to initiate app test: %s", err)
+		}
+		assert.Equal(t, fiber.StatusInternalServerError, res.StatusCode)
+	})
+}
+
+func TestCreateMeeting_FailedCausedByBBB(t *testing.T) {
+	// prepare fake server to mimic BBB Server
+	var fakeFailedServer = func(t *testing.T) *httptest.Server {
+		return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			resp := struct {
+				Name string `json:"name"`
+			}{}
+			js, err := json.Marshal(&resp)
+			if err != nil {
+				t.Fatalf("failed to marshal object to xml: %s", err)
+			}
+
+			rw.WriteHeader(fiber.StatusCreated)
+			rw.Header().Set("Content-Type", fiber.MIMEApplicationJSON)
+			_, err = rw.Write(js)
+			require.NoError(t, err)
+		}))
+	}
+
+	conf, err := config.NewConfig(bytes.NewBufferString(sampleConfigFile[0]))
+	require.NoError(t, err)
+	require.NoError(t, conf.Sanitization())
+	conf.BBB.Host = fakeFailedServer(t).URL
+	require.NoError(t, conf.BBB.Sanitization())
+
+	app := fiber.New()
+	app.Post("/meeting", CreateMeeting(conf, fakeFailedServer(t).Client()))
+
+	t.Run("Failed if BBB server send response back using content type other than xml", func(t *testing.T) {
+		buf := bytes.NewBufferString(sampleRequestBody[0])
+		req := httptest.NewRequest(fiber.MethodPost, "/meeting", buf)
+		req.Header.Set("Content-Type", fiber.MIMEApplicationJSON)
+		res, err := app.Test(req)
+		if err != nil {
+			t.Fatalf("failed to initiate app test: %s", err)
+		}
+		assert.Equal(t, fiber.StatusBadGateway, res.StatusCode)
 	})
 }
